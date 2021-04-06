d292ed13e31dd3b68ae913b195bea7347c15d897
hamzaelkaiche <fewdiefie@hotmail.com>
2021-04-06 22:58:16 +0100

    tp1/ex2.cpp

--- tp1/ex2.cpp
+++ tp1/ex2.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 class CPoint {
+    double x, y;
+
 public:
     CPoint() {
         x = y = 0;
@@ -34,10 +36,69 @@ public:
         double dx = this->x - x, dy = this->y - y;
         return sqrt(dx * dx + dy * dy);
     }
-
-private:
-    double x, y;
 };
+
+class Triangle {
+    CPoint p1, p2, p3;
+    static int nb;
+
+public:
+    Triangle() {
+        nb++;
+    }
+
+    int compteur() {
+        return nb;
+    }
+
+    void saisie() {
+        cout << "point 1: "; p1.saisie();
+        cout << "point 2: "; p2.saisie();
+        cout << "point 3: "; p3.saisie();
+    }
+
+    void afficher() {
+        cout << "point 1: "; p1.afficher();
+        cout << "point 2: "; p2.afficher();
+        cout << "point 3: "; p3.afficher();
+    }
+
+    void translater(double dx, double dy) {
+        p1.translater(dx, dy);
+        p2.translater(dx, dy);
+        p3.translater(dx, dy);
+    }
+
+    double cote1() {
+        return p1.distance(p2.getAbscisse(), p2.getOrdonne());
+    }
+
+    double cote2() {
+        return p2.distance(p3.getAbscisse(), p3.getOrdonne());
+    }
+
+    double cote3() {
+        return p3.distance(p1.getAbscisse(), p1.getOrdonne());
+    }
+
+    bool isocele() {
+        double c1 = cote1(),
+               c2 = cote2(),
+               c3 = cote3();
+        return c1 == c2 || c1 == c3 || c2 == c3;
+    }
+
+    bool equilateral() {
+        double c1 = cote1();
+        return c1 == cote2() && c1 == cote3();
+    }
+
+    double perimetre() {
+        return cote1() + cote2() + cote3();
+    }
+};
+
+int Triangle::nb = 0;
 
 int main() {
     CPoint p1;
@@ -51,4 +112,7 @@ int main() {
     cout << "p2 = "; p2.afficher();
 
     cout << "distance(p1, p2) = " << p1.distance(p2.getAbscisse(), p2.getOrdonne()) << endl;
+
+    Triangle t1, t2, t3;
+    cout << "nombre de triangle = " << t1.compteur() << endl;
 }
